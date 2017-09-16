3763ef495a52d4b13b1c8736402ef2504a9d2058
Sandor Rozsa <rozsa@sharksinn.com>
2017-09-16 03:44:58 +0200

    Typography panel added

--- js/controller.js
+++ js/controller.js
@@ -233,40 +233,40 @@ var customProperties = 'name icon'.split(' ');
 
 
 function copy() {
-// clone what are you copying since you may want copy and paste on different moment.
-// and you do not want the changes happened later to reflect on the copy.
-// maybe.
-  canvas.getActiveObject().clone(function(cloned) {
-    _clipboard = cloned;
-  },customProperties);
+    // clone what are you copying since you may want copy and paste on different moment.
+    // and you do not want the changes happened later to reflect on the copy.
+    // maybe.
+    canvas.getActiveObject().clone(function (cloned) {
+        _clipboard = cloned;
+    }, customProperties);
 }
 
 function paste() {
-// clone again, so you can do multiple copies.
-  _clipboard.clone(function(clonedObj) {
-    canvas.discardActiveObject();
-    clonedObj.set({
-      left: clonedObj.left + 10,
-      top: clonedObj.top + 10,
-      evented: true,
-    });
-    if (clonedObj.type === 'activeSelection') {
-        // active selection needs a reference to the canvas.
-        clonedObj.canvas = canvas;
-        clonedObj.forEachObject(function (obj) {
-            canvas.add(obj);
+    // clone again, so you can do multiple copies.
+    _clipboard.clone(function (clonedObj) {
+        canvas.discardActiveObject();
+        clonedObj.set({
+            left: clonedObj.left + 10,
+            top: clonedObj.top + 10,
+            evented: true,
         });
-        // this should solve the unselectability
-        clonedObj.setCoords();
-    } else {
-       canvas.add(clonedObj);
-    }
-    canvas.setActiveObject(clonedObj);
-    canvas.requestRenderAll();
-    console.log(clonedObj);
-    
-    _clipboard = clonedObj;
- },customProperties);
+        if (clonedObj.type === 'activeSelection') {
+            // active selection needs a reference to the canvas.
+            clonedObj.canvas = canvas;
+            clonedObj.forEachObject(function (obj) {
+                canvas.add(obj);
+            });
+            // this should solve the unselectability
+            clonedObj.setCoords();
+        } else {
+            canvas.add(clonedObj);
+        }
+        canvas.setActiveObject(clonedObj);
+        canvas.requestRenderAll();
+        console.log(clonedObj);
+
+        _clipboard = clonedObj;
+    }, customProperties);
 }
 
 function remove() {
@@ -1920,7 +1920,7 @@ kitchensink.controller('RightTabsCtrl', ['$scope', function ($scope) {
 
   ];
 
-    $scope.currentTab = 'templates/style_menu.html';
+    $scope.currentTab = 'templates/actions_menu.html';
 
     $scope.onClickTab = function (tab) {
         $scope.currentTab = tab.url;
@@ -1952,3 +1952,218 @@ kitchensink.controller('LeftTabsCtrl', ['$scope', function ($scope) {
     }
 
 }])
+
+kitchensink.controller('FontsCtrl', function ($scope) {
+    $scope.fonts = [
+        {
+            name: 'American Typewriter',
+            value: "american typewriter",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Arial',
+            value: "arial",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Arial Rounded MT Bold',
+            value: "Arial Rounded MT Bold",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Avenir',
+            value: "Avenir",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Avenir Next',
+            value: "Avenir Next",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Avenir Next Condensed',
+            value: "Avenir Next Condensed",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Baskerville',
+            value: "Baskerville",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Bodoni 72',
+            value: "Bodoni 72",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Bodoni 72 Oldstyle',
+            value: "Bodoni 72 Oldstyle",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Bodoni 72 Smallcaps',
+            value: "Bodoni 72 Smallcaps",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Bodoni Ornaments',
+            value: "Bodoni Orvaluents",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Bradly Hand Bold',
+            value: "Bradly Hand Bold",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Chalkboard SE',
+            value: "Chalkboard SE",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Chalkduster',
+            value: "Chalkduster",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Cochin',
+            value: "Cochin",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Copperplate',
+            value: "Copperplate",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Courier',
+            value: "Courier",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Courier New',
+            value: "Courier New",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Futura',
+            value: "Futura",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Geeza Pro',
+            value: "Geeza Pro",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Georgia',
+            value: "Georgia",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Gill Sans',
+            value: "Gill Sans",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Helvetica',
+            value: "Helvetica",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Helvetica Neue',
+            value: "Helvetica Neue",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Marker Felt',
+            value: "Marker Felt",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Optima',
+            value: "Optima",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Palatino',
+            value: "Palatino",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Papyrus',
+            value: "Papyrus",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Papyrus Condensed',
+            value: "Papyrus Condensed",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Symbol',
+            value: "Symbol",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Times New Roman',
+            value: "Times New Roman",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Trebuchet MS',
+            value: "Trebuchet MS",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Verdana',
+            value: "Verdana",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Zapf Dingbats',
+            value: "Zapf Dingbats",
+            type: 'iOS'
+        },
+
+        {
+            name: 'Zapfino',
+            value: "Zapfino",
+            type: 'iOS'
+        }
+
+  ];
+});
