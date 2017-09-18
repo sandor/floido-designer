b60ac6da652fb4ead8703587bc7056b2b2377856
Sandor Rozsa <rozsa@sharksinn.com>
2017-09-18 19:16:38 +0200

    action menu

--- main.js
+++ main.js
@@ -13,14 +13,16 @@ app.on("ready", () => {
 
 
 const electron = require('electron')
-	// Module to control application life.
+    // Module to control application life.
 const app = electron.app
-	// Module to create native browser window.
+    // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
-const {ipcMain} = require('electron')
+const {
+    ipcMain
+} = require('electron')
 
 const reload = require('electron-reload')(__dirname)
 
@@ -28,63 +30,68 @@ const reload = require('electron-reload')(__dirname)
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+
+
 function createWindow() {
-	// Create the browser window.
-	mainWindow = new BrowserWindow({
-		width: 1366,
-		height: 768,
-		titleBarStyle: 'hidden',
-		vibrancy: 'medium-light'
-	})
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 1366,
+        height: 768,
+        titleBarStyle: 'hidden',
+        vibrancy: 'medium-light'
+    })
 
-	// and load the index.html of the app.
-	mainWindow.loadURL(url.format({
-		pathname: path.join(__dirname, 'index.html'),
-		protocol: 'file:',
-		slashes: true
-	}))
 
-	mainWindow.webContents.on('did-finish-load', function () {
-		mainWindow.webContents.send('ping', 'whoooooooh!');
-	})
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
 
-	// Open the DevTools.
-	// mainWindow.webContents.openDevTools()
+    mainWindow.webContents.on('did-finish-load', function () {
+        mainWindow.webContents.send('ping', 'whoooooooh!');
+    })
 
-	// Emitted when the window is closed.
-	mainWindow.on('closed', function () {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
-		mainWindow = null
-	})
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
 
-	require('./menu/mainmenu')
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
+
+    require('./menu/mainmenu')
 
 }
 
 ipcMain.on('openFile', (event, path) => {
-      const {dialog} = require('electron')
-      const fs = require('fs')
-      dialog.showOpenDialog(function (fileNames) {
-            // fileNames is an array that contains all the selected
-          if(fileNames === undefined){
-                  console.log("No file selected");
-          }else{
-                  readFile(fileNames[0]);
-          }
-      });
+    const {
+        dialog
+    } = require('electron')
+    const fs = require('fs')
+    dialog.showOpenDialog(function (fileNames) {
+        // fileNames is an array that contains all the selected
+        if (fileNames === undefined) {
+            console.log("No file selected");
+        } else {
+            readFile(fileNames[0]);
+        }
+    });
 
-      function readFile(filepath){
-         fs.readFile(filepath, 'utf-8', (err, data) => {
-            if(err){
-               alert("An error ocurred reading the file :" + err.message)
-               return
+    function readFile(filepath) {
+        fs.readFile(filepath, 'utf-8', (err, data) => {
+            if (err) {
+                alert("An error ocurred reading the file :" + err.message)
+                return
             }
             // handle the file content
             event.sender.send('fileData', filepath)
-      })
-      }
+        })
+    }
 })
 
 
@@ -96,19 +103,19 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-	// On OS X it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin') {
-		app.quit()
-	}
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', function () {
-	// On OS X it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (mainWindow === null) {
-		createWindow()
-	}
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
