d2b96b1e4e490480672ae0b2cc0ddbdedbd3d153
Sandor Rozsa <rozsa@sharksinn.com>
2017-09-26 13:52:29 +0200

    left tab bar icons

--- js/controllers/kitchensinkController/LeftTabsController.js
+++ js/controllers/kitchensinkController/LeftTabsController.js
@@ -1,12 +1,12 @@
 kitchensink.controller('LeftTabsCtrl', ['$scope', function ($scope) {
     $scope.tabs = [{
-            title: 'One',
+            title: 'Layers',
             url: 'templates/layers.html',
             micon: 'layers',
             custom: 'images/icons.svg'
     },
-				   {
-    title: 'Two',
+        {
+            title: 'Pages',
             url: 'templates/pages.html',
             micon: 'pages',
             custom: 'images/icons.svg'
